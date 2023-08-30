bd29078a3b943025351ceb5067c735a0be4f0a23
unknown <HibaAbbas13>
2023-08-30 21:19:05 +0500

    Update

--- lib/view/auth/Signup_page.dart
+++ lib/view/auth/Signup_page.dart
@@ -39,7 +39,7 @@ class _SignupPageState extends State<SignupPage> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _passwordController1 = TextEditingController();
-  final _passwordController2 = TextEditingController();
+
   var password;
   @override
   Widget build(BuildContext context) {
